0ab2ef9f69973632c2b70776bf405eb585e04498
wetson <729532485@qq.com>
2017-04-25 15:55:37 +0800

    Update and rename plugins/category_plugin.rb to _plugins/category_plugin.rb

--- _plugins/category_plugin.rb
+++ _plugins/category_plugin.rb
@@ -1,6 +1,6 @@
 module Jekyll
 
-  class CategoryIndex < Page
+  class CategoryPage < Page
     def initialize(site, base, dir, category)
       @site = site
       @base = base
@@ -11,28 +11,21 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
       self.data['category'] = category
 
-      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+      category_title_prefix = site.config['category_title_prefix'] || ''
       self.data['title'] = "#{category_title_prefix}#{category}"
     end
   end
 
-  class CategoryGenerator < Generator
+  class CategoryPageGenerator < Generator
     safe true
-    
+
     def generate(site)
       if site.layouts.key? 'category_index'
         dir = site.config['category_dir'] || 'categories'
         site.categories.keys.each do |category|
-          write_category_index(site, File.join(dir, category), category)
+          site.pages << CategoryPage.new(site, site.source, File.join(dir, category), category)
         end
       end
-    end
-  
-    def write_category_index(site, dir, category)
-      index = CategoryIndex.new(site, site.source, dir, category)
-      index.render(site.layouts, site.site_payload)
-      index.write(site.dest)
-      site.pages << index
     end
   end
 
